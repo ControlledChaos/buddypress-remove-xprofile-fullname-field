0f6b3c22bc29680032ac96fc58993b8b929d28d5
Daan Kortenbach <daan@kortenbach.org>
2013-04-10 12:31:31 +0200

    improved version

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This plugin removes the default BuddyPress xprofile fullname field.
+This plugin removes the default BuddyPress xprofile fullname field on the register page and sets fullname to user_login.
--- buddypress-remove-xprofile-fullname-field.php
+++ buddypress-remove-xprofile-fullname-field.php
@@ -1,14 +1,14 @@
 <?php
 /**
-* This plugin removes the default BuddyPress xprofile fullname field
+* This plugin removes the default BuddyPress xprofile fullname field on the register page and sets fullname to user_login
 *
 * @author Daan Kortenbach
 *
 * Plugin Name: BuddyPress remove xprofile fullname field
 * Plugin URI: http://daan.kortenba.ch/
-* Description: This plugin removes the default BuddyPress xprofile fullname field
+* Description: This plugin removes the default BuddyPress xprofile fullname field on the register page and sets fullname to user_login
 * Author: Daan Kortenbach
-* Version: 0.2
+* Version: 0.3
 * Author URI: http://daan.kortenba.ch/
 * License: GPLv2
 */
@@ -37,32 +37,50 @@ function dk_bp_remove_xprofile_fullname_field( $fields ){
 	return $fields;
 }
 
-add_action( 'bp_core_activated_user', 'dk_bp_set_user_nicename', 9999 );
+add_action( 'bp_custom_profile_edit_fields', 'dk_bp_add_xprofile_fullname_field_hidden' );
 /**
- * Resets user because it got the wrong user meta (WTF BuddyPress, why?).
- * See xprofile_sync_wp_profile() in BuddyPress.
+ * Adds the default BuddyPress xprofile fullname field as a hidden field
+ *
+ * @return string
+ *
+ * @author  Daan Kortenbach
+ */
+function dk_bp_add_xprofile_fullname_field_hidden(){
+
+	if( ! bp_is_register_page() )
+		return;
+
+	echo '<input type="hidden" name="field_1" id="field_1" value="x">';
+}
+
+
+
+
+add_action( 'bp_core_signup_user', 'dk_bp_core_signup_user' );
+/**
+ * Sets the correct user values
  *
  * @param  integer $user_id
  * @return void
  *
- * @author Daan Kortenbach
+ * @author  Daan Kortenbach
  */
-function dk_bp_set_user_nicename( $user_id ){
+function dk_bp_core_signup_user( $user_id ) {
 
 	// Get user data
 	$user = get_userdata( $user_id );
+
+	// Update xprofile fullname
+	xprofile_set_field_data( BP_XPROFILE_FULLNAME_FIELD_NAME, $user_id, $user->user_login );
 
 	// Set new user data
 	$userdata = array(
 		'ID' => $user_id,
 		'user_nicename' => $user->user_login,
 		'display_name' => $user->user_login,
-		'nickname' => $user->user_login,
-		'first_name' => $user->user_login
+		'nickname' => $user->user_login
 	);
+
 	// Update user
 	wp_update_user( $userdata );
-
-	// Update xprofile
-	xprofile_set_field_data( BP_XPROFILE_FULLNAME_FIELD_NAME, $user_id, $user->user_login );
 }
